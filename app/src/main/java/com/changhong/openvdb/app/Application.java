4bbb82429bab4f0521f7762cbb445c4ccd9ba649
luotiansheng <redgogh@vip.qq.com>
2026-04-14 14:38:36 +0800

    app: 切换复古风格主题

--- app/src/main/java/com/changhong/openvdb/app/Application.java
+++ app/src/main/java/com/changhong/openvdb/app/Application.java
@@ -83,7 +83,7 @@ public class Application extends javafx.application.Application
         {
                 primaryStage = stage;
 
-                javafx.application.Application.setUserAgentStylesheet(new CupertinoLight().getUserAgentStylesheet());
+                // javafx.application.Application.setUserAgentStylesheet(new CupertinoLight().getUserAgentStylesheet());
                 Scene scene = new Scene(new MainLayout(), 1200, 800);
                 initializeVFX(scene);
                 stage.setTitle(TITLE);
